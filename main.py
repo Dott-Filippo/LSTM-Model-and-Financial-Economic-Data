9eb20d33d54e7894567f1a8752045248ccde2e7b
Dott-Filippo <filippocavanna.fc@gmail.com>
2024-09-20 18:37:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import pandas as pd
 import yfinance as yf
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from sklearn.preprocessing import MinMaxScaler
